524848925734a8a2c375511bcfdf31ddd3bf4dbb
eror202 <yura211252@gmail.com>
2024-03-26 20:58:48 +0300

    Add the getter and setter

--- field.go
+++ field.go
@@ -9,3 +9,17 @@ func (field Field) Width() int {
 func (field Field) Height() int {
 	return len(field)
 }
+
+func (field Field) Cell(column int, row int) bool {
+	column = wrapAroundModulus(column, field.Width())
+	row = wrapAroundModulus(row, field.Height())
+	return field[row][column]
+}
+
+func (field Field) SetCell(column int, row int, cell bool) {
+	field[row][column] = cell
+}
+
+func wrapAroundModulus(value int, modulus int) int {
+	return (value + modulus) % modulus
+}
--- field_test.go
+++ field_test.go
@@ -25,3 +25,47 @@ func TestField_Height(test *testing.T) {
 
 	assert.Equal(test, 2, result)
 }
+
+func TestField_Cell(test *testing.T) {
+	field := Field{
+		{false, false, false},
+		{false, false, true},
+	}
+	cell := field.Cell(2, 1)
+
+	assert.True(test, cell)
+}
+
+func TestField_Cell_withCoordinatesBeyondMinimum(test *testing.T) {
+	field := Field{
+		{false, false, false},
+		{false, true, false},
+	}
+	cell := field.Cell(-2, -1)
+
+	assert.True(test, cell)
+}
+
+func TestField_Cell_withCoordinatesBeyondMaximum(test *testing.T) {
+	field := Field{
+		{false, false, false},
+		{false, true, false},
+	}
+	cell := field.Cell(4, 3)
+
+	assert.True(test, cell)
+}
+
+func TestField_SetCell(test *testing.T) {
+	field := Field{
+		{false, false, false},
+		{false, false, false},
+	}
+	field.SetCell(2, 1, true)
+
+	expectedField := Field{
+		{false, false, false},
+		{false, false, true},
+	}
+	assert.Equal(test, expectedField, field)
+}
